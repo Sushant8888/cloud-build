968453c0defe4629b85dfd091e2382796ba28a67
root <sushant8888>
2020-07-10 20:28:03 +0530

    commit everything

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import torch
 x = torch.rand(2,2)
 y = torch.rand(2,2)
+print(x)
+print(y)
 y.add(x)
 print("FINAL RESULTS:",y)
+print("FINAL RESULTS:",y)
+print("FINAL RESULTS:",y)
+
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
