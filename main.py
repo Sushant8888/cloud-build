d090d8d9ff6e666cfd4ca35e469711d4bb793f71
Sushant Nigudkar <50460190+Sushant8888@users.noreply.github.com>
2020-07-10 19:13:37 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import torch
+x = torch.rand(2,2)
+y = torch.rand(2,2)
+y.add(x)
+print("FINAL RESULTS:",y)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+print(device)
+print(device)
+print(device)
+print(device)
+print(device)
+print(device)
